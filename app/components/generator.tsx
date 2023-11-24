d62f0d3944d3f4095b679c2edc5791bbb4e09be5
Eze Gatica <git@ezegatica.com>
2023-11-23 23:20:36 -0300

    v1.0!

---
+++ app/components/generator.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Image from 'next/image';
+export default function Generator() {
+  return (
+    <div className="generator">
+      <div className="flex justify-center items-center mt-5 mb-8">
+        <Image src="/bg.svg" width={125} height={125} alt="" />
+      </div>
+      <p className="text-center uppercase font-semibold text-xl mb-10">
+        Comunicado de Prensa
+      </p>
+      <div className="text-lg" contentEditable="plaintext-only">
+        <p>
+          Ciudad de Buenos Aires, 23 de noviembre de 2023.- El Presidente
+          Electo, Javier Milei, encabezó esta mañana una reunión con sus equipos
+          técnicos. <br />
+          Lo acompañaron Nicolás Posse, Diana Mondino, Guillermo Francos y
+          Karina Milei. <br /> <br />
+          Por otro lado, el Presidente electo recibió anoche el llamado del ex
+          Presidente de los Estados Unidos, Donald Trump, quien lo felicitó y
+          señaló que el triunfo por amplia diferencia en la elección del domingo
+          pasado tuvo gran impacto a nivel mundial. <br /> <br />
+          Asimismo, el ex Presidente Trump anticipó que viajará a la Ciudad de
+          Buenos Aires para reunirse con él.
+        </p>
+      </div>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Tinos } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const tinos = Tinos({ subsets: ['latin'], weight: ['400', '700'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={tinos.className}>{children}</body>
     </html>
   )
 }
