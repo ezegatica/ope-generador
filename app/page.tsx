57d4c3dafd5daab8857d408ae8cc91923f858971
Nicolás Halperin <69744829+NicoHalpe@users.noreply.github.com>
2023-11-24 20:00:57 -0300

    Altura adaptable (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ import Generator from './components/generator';
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div className="mx-auto max-w-xl">
+    <div className="w-full h-full flex flex-col">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <Generator />
-        <div className="footer">
-          <hr />
-          <p className="text-center mt-2 text-lg">
-            Oficina del Presidente electo de la República Argentina
-          </p>
-        </div>
+      </div>
+      <div className="footer">
+        <hr />
+        <p className="text-center mt-2 text-lg">
+          Oficina del Presidente electo de la República Argentina
+        </p>
       </div>
     </div>
   );
